8f4bf9f1dbc190b7780a4390e56205fb38dce534
Andrew Wheeler <wheeler02@gmail.com>
2015-10-11 13:08:07 -0700

    mod slider timing

--- javascripts/app.js
+++ javascripts/app.js
@@ -258,7 +258,7 @@ jQuery(document).foundation();
         pauseOnHover: false,
         dots: true,
         speed: 1500,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 15000,
         arrows: false
       });
 
